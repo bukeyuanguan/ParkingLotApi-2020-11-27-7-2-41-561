c7c9930bac172a3c0c2f2f024d1c066a9a84deaa
Ge Luxue <lge5@slb.com>
2020-11-29 22:02:49 +0800

    test: uncomment test for update parkinglot, can not add parking lot when name exist

--- ParkingLotApiTest/ControllerTest/ParkingLotControllerTest.cs
+++ ParkingLotApiTest/ControllerTest/ParkingLotControllerTest.cs
@@ -99,20 +99,20 @@ namespace ParkingLotApiTest.ControllerTest
             Assert.Equal(1, context.ParkingLots.Count());
         }
 
-        //[Fact]
-        //public async Task Should_not_add_parkingLot_when_name_already_exist_via_service()
-        //{
-        //    var scope = Factory.Services.CreateScope();
-        //    var scopedServices = scope.ServiceProvider;
-        //    ParkingLotDbContext context = scopedServices.GetRequiredService<ParkingLotDbContext>();
-        //    context.ParkingLots.RemoveRange(context.ParkingLots);
-        //    context.SaveChanges();
-        //    ParkingLotService parkingLotService = new ParkingLotService(context);
-        //    await parkingLotService.DeleteAllParkingLot();
-        //    await parkingLotService.AddParkingLot(parkingLotDto1);
-        //    //await parkingLotService.AddParkingLot(parkingLotDto1);
-        //    Assert.Equal(1, context.ParkingLots.Count());
-        //}
+        [Fact]
+        public async Task Should_not_add_parkingLot_when_name_already_exist_via_service()
+        {
+            var scope = Factory.Services.CreateScope();
+            var scopedServices = scope.ServiceProvider;
+            ParkingLotDbContext context = scopedServices.GetRequiredService<ParkingLotDbContext>();
+            context.ParkingLots.RemoveRange(context.ParkingLots);
+            context.SaveChanges();
+            ParkingLotService parkingLotService = new ParkingLotService(context);
+            await parkingLotService.DeleteAllParkingLot();
+            await parkingLotService.AddParkingLot(parkingLotDto1);
+            //await parkingLotService.AddParkingLot(parkingLotDto1);
+            Assert.Equal(1, context.ParkingLots.Count());
+        }
 
         [Fact]
         public async Task Should_get_parkingLot_Successfully_by_name_Via_Service()
@@ -163,21 +163,21 @@ namespace ParkingLotApiTest.ControllerTest
             Assert.Equal(1, context.ParkingLots.Count());
         }
 
-        //[Fact]
-        //public async Task Should_update_parkingLot_Successfully_Via_Service()
-        //{
-        //    var scope = Factory.Services.CreateScope();
-        //    var scopedServices = scope.ServiceProvider;
+        [Fact]
+        public async Task Should_update_parkingLot_Successfully_Via_Service()
+        {
+            var scope = Factory.Services.CreateScope();
+            var scopedServices = scope.ServiceProvider;
 
-        //    ParkingLotDbContext context = scopedServices.GetRequiredService<ParkingLotDbContext>();
-        //    ParkingLotService parkingLotService = new ParkingLotService(context);
-        //    context.ParkingLots.RemoveRange(context.ParkingLots);
-        //    context.SaveChanges();
-        //    var update = new UpdateParkingLotDto(50);
-        //    var addReturn = await parkingLotService.AddParkingLot(parkingLotDto1);
-        //    var updateReturn = await parkingLotService.UpdateParkingLot(addReturn, update);
-        //    Assert.Equal(50, updateReturn.Capacity);
-        //}
+            ParkingLotDbContext context = scopedServices.GetRequiredService<ParkingLotDbContext>();
+            ParkingLotService parkingLotService = new ParkingLotService(context);
+            context.ParkingLots.RemoveRange(context.ParkingLots);
+            context.SaveChanges();
+            var update = new UpdateParkingLotDto(50);
+            var addReturn = await parkingLotService.AddParkingLot(parkingLotDto1);
+            var updateReturn = await parkingLotService.UpdateParkingLot(addReturn, update);
+            Assert.Equal(50, updateReturn.Capacity);
+        }
 
         [Fact]
         public async Task Should_get_all_parkingLots_Via_Service()
@@ -365,34 +365,34 @@ namespace ParkingLotApiTest.ControllerTest
             Assert.Equal(1, respondParkingLot.Count);
         }
 
-        //[Fact]
-        //public async Task Should_update_parkingLot_when_update()
-        //{
-        //    this.client.DeleteAsync("/parkingLots/clear");
-        //    //given
-        //    var request1 = JsonConvert.SerializeObject(parkingLotDto1);
-        //    var request2 = JsonConvert.SerializeObject(parkingLotDto2);
-        //    StringContent requestBody1 = new StringContent(request1, Encoding.UTF8, "application/json");
-        //    StringContent requestBody2 = new StringContent(request2, Encoding.UTF8, "application/json");
-        //    var response1 = await client.PostAsync("/parkingLots", requestBody1);
-        //    var response2 = await client.PostAsync("/parkingLots", requestBody2);
+        [Fact]
+        public async Task Should_update_parkingLot_when_update()
+        {
+            this.client.DeleteAsync("/parkingLots/clear");
+            //given
+            var request1 = JsonConvert.SerializeObject(parkingLotDto1);
+            var request2 = JsonConvert.SerializeObject(parkingLotDto2);
+            StringContent requestBody1 = new StringContent(request1, Encoding.UTF8, "application/json");
+            StringContent requestBody2 = new StringContent(request2, Encoding.UTF8, "application/json");
+            var response1 = await client.PostAsync("/parkingLots", requestBody1);
+            var response2 = await client.PostAsync("/parkingLots", requestBody2);
 
-        //    var responseBody1 = await response1.Content.ReadAsStringAsync();
-        //    ParkingLotDto addParkingLot1 = JsonConvert.DeserializeObject<ParkingLotDto>(responseBody1);
-        //    var name = addParkingLot1.Name;
-        //    var update = new UpdateParkingLotDto(50);
-        //    parkingLotDto1.Capacity = update.Capacity;
-        //    var updateRequest = JsonConvert.SerializeObject(update);
-        //    StringContent updateRequestBody = new StringContent(updateRequest, Encoding.UTF8, "application/json");
-        //    //when
-        //    var response = await client.PatchAsync($"/parkingLots/{name}", updateRequestBody);
-        //    var responseBody = await response.Content.ReadAsStringAsync();
-        //    ParkingLotDto respondParkingLot = JsonConvert.DeserializeObject<ParkingLotDto>(responseBody);
-        //    //then
-        //    var parkingLotDto1String = JsonConvert.SerializeObject(parkingLotDto1);
-        //    var respondParkingLotString = JsonConvert.SerializeObject(respondParkingLot);
-        //    Assert.Equal(parkingLotDto1String, respondParkingLotString);
-        //}
+            var responseBody1 = await response1.Content.ReadAsStringAsync();
+            ParkingLotDto addParkingLot1 = JsonConvert.DeserializeObject<ParkingLotDto>(responseBody1);
+            var name = addParkingLot1.Name;
+            var update = new UpdateParkingLotDto(50);
+            parkingLotDto1.Capacity = update.Capacity;
+            var updateRequest = JsonConvert.SerializeObject(update);
+            StringContent updateRequestBody = new StringContent(updateRequest, Encoding.UTF8, "application/json");
+            //when
+            var response = await client.PatchAsync($"/parkingLots/{name}", updateRequestBody);
+            var responseBody = await response.Content.ReadAsStringAsync();
+            ParkingLotDto respondParkingLot = JsonConvert.DeserializeObject<ParkingLotDto>(responseBody);
+            //then
+            var parkingLotDto1String = JsonConvert.SerializeObject(parkingLotDto1);
+            var respondParkingLotString = JsonConvert.SerializeObject(respondParkingLot);
+            Assert.Equal(parkingLotDto1String, respondParkingLotString);
+        }
 
         [Fact]
         public async Task Should_delete_parkingLot_when_delete()
